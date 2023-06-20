4260d8e69392907dddc26ea6aa59715a43ca50f0
Nikita531 <110817057+Nikita531@users.noreply.github.com>
2023-06-20 14:53:47 +0300

    Animations
    Переключение анимации по клику.

---
+++ My project (1)/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnMouseDown()
+    {
+        // Здесь вы можете выполнить любую логику перед запуском анимации,
+        // если это необходимо.
+
+        animator.SetTrigger("TriggerName");
+    }
+}
+
+
